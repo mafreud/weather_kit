21d07a0c3e1fba3b020e993a0a0d511cd11bf310
Masahiro Aoki <rafe.kun@gmail.com>
2022-09-24 17:44:25 +0900

    update

---
+++ example/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather Kit DEMO',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('weather kit demo'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              //
+            },
+            child: const Text('Generate JWT'),
+          )
+        ],
+      )),
+    );
+  }
+}
--- lib/weather_kit.dart
+++ lib/weather_kit.dart
@@ -1,7 +1,64 @@
 library weather_kit;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:io';
+
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherKit {
+  String generateJWT({
+    required String bundleId,
+    required String teamId,
+    required String keyId,
+    required String pem,
+    required Duration expiresIn,
+  }) {
+    final jwt = JWT(
+      {
+        'sub': bundleId,
+      },
+      issuer: teamId,
+      header: {
+        "typ": "JWT",
+        'id': "$teamId.$bundleId",
+        'alg': 'ES256',
+        'kid': keyId,
+      },
+    );
+    final token = jwt.sign(
+      ECPrivateKey(
+        pem,
+      ),
+      algorithm: JWTAlgorithm.ES256,
+      expiresIn: expiresIn,
+    );
+    return token;
+  }
+
+  Future<http.Response> fetchAvailability({
+    required String jwt,
+    required String latitude,
+    required String longitude,
+  }) async {
+    final response = await http.get(
+      Uri.parse(
+          "https://weatherkit.apple.com/api/v1/availability/$latitude/$longitude"),
+      headers: {HttpHeaders.authorizationHeader: jwt},
+    );
+    return response;
+  }
+
+  Future<http.Response> obtainWeatherData(
+      {required String jwt,
+      required String language,
+      required String latitude,
+      required String longitude,
+      required String dataSets}) async {
+    final response = await http.get(
+      Uri.parse(
+          "https://weatherkit.apple.com/api/v1/weather/$language/$latitude/$longitude?dataSets=$dataSets"),
+      headers: {HttpHeaders.authorizationHeader: jwt},
+    );
+    return response;
+  }
 }
