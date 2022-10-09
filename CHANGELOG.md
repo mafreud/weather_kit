4188e49b1867abac9226a0f4a089ae8b3b04d8f8
Masahiro Aoki <rafe.kun@gmail.com>
2022-10-09 23:31:40 +0900

    refactor

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.0
+
+- refactor
+
 ## 0.1.1
 
 - add pricing section to README
---
+++ lib/src/models/pressure_trend.dart
@@ -0,0 +1,11 @@
+/// The direction of change of the sea level air pressure.
+enum PressureTrend {
+  /// The sea level air pressure is increasing.
+  rising,
+
+  /// The sea level air pressure is decreasing.
+  falling,
+
+  /// The sea level air pressure is remaining about the same.
+  steady
+}
